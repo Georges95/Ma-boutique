bba3a7f6c64817f83cc7c629f59c6a2d2048d31d
Georges <georges.laerche@gmail.com>
2025-09-17 17:38:53 +0200

    creation du login et avancement sur modification du MdP

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\PasswordUserType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class AccountController extends AbstractController
+{
+    #[Route('/compte', name: 'app_account')]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig');
+    }
+
+    #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
+    public function password(): Response
+    {
+        $form = $this->createForm(PasswordUserType::class);
+
+        return $this->render('account/password.html.twig',[
+            'modifyPwd' => $form->createView()
+        ]);
+    }
+}
